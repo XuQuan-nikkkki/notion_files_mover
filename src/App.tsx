20a8c81ec42c68b6409bb08974bab7ffe6c66011
Xu Quan <xuquan1225@hotmail.com>
2024-10-03 18:06:51 +0800

    feat: support both English and Chinese

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React, { CSSProperties } from "react";
-import { Layout } from "antd";
+import React, { CSSProperties, useState } from "react";
+import { Layout, Radio } from "antd";
+
+import { Language, LANGUAGE_MAP, LANGUAGES, TITLE_MAP } from "./constant";
 
 const { Header } = Layout;
+
+const DefaultLanguage: Language = "English";
 
 const layoutStyle: CSSProperties = {
   height: "100vh",
@@ -17,10 +21,32 @@ const headerStyle: CSSProperties = {
 };
 
 const App = () => {
+  const [language, setLanguage] = useState<Language>(DefaultLanguage);
+
+  const renderLanguageSelector = () => {
+    return (
+      <Radio.Group size="small" defaultValue={language}>
+        {LANGUAGES.map((lang: Language) => (
+          <Radio.Button
+            key={lang}
+            value={lang}
+            checked={lang === language}
+            onChange={() => setLanguage(lang)}
+          >
+            {LANGUAGE_MAP[lang]}
+          </Radio.Button>
+        ))}
+      </Radio.Group>
+    );
+  };
+
   return (
     <div className="app">
       <Layout style={layoutStyle}>
-        <Header style={headerStyle}>Notion Files Transfer Tool</Header>
+        <Header style={headerStyle}>
+          {TITLE_MAP[language]}
+          {renderLanguageSelector()}
+        </Header>
       </Layout>
     </div>
   );
